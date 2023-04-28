577d2a8aaa01d972e442ebafee4a07b65ca27c80
NeilEllison <ellison.neil1@gmail.com>
2023-04-28 09:47:59 -0700

    small change.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
             <form>
               <label htmlFor="address">Enter your address:</label>
               <div className="mb-3">
-                <input type="text" name="address" className="form-control" id="address" placeholder="123 rd..." />
+                <input type="text" name="address" className="form-control" id="address" placeholder="123 rd.." />
               </div>
               <button type="submit" className="btn btn-outline-dark">
                 Find closest locations
